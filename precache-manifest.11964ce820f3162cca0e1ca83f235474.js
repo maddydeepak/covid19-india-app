b20ad336bab696a91a6de400d89780d55b35c318
maddydeepak <maddheshiya.deepak@gmail.com>
2020-04-11 21:05:23 +0530

    Updates

---
+++ precache-manifest.11964ce820f3162cca0e1ca83f235474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bffc639780eeba0609101a745126019",
+    "url": "/covid19-india-app/index.html"
+  },
+  {
+    "revision": "9281ef8b4e83fba1d00c",
+    "url": "/covid19-india-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b8ef379021c529c3449",
+    "url": "/covid19-india-app/static/js/2.508f962d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-india-app/static/js/2.508f962d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9281ef8b4e83fba1d00c",
+    "url": "/covid19-india-app/static/js/main.c18cd5e4.chunk.js"
+  },
+  {
+    "revision": "36b1b40e637735cf0a07",
+    "url": "/covid19-india-app/static/js/runtime-main.853fe636.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-india-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
